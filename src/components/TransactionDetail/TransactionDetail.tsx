6389f11074856417a159e4be7446d7c15f4b5cd3
v3nk3y <nagireddyvenkatesh@gmail.com>
2026-09-10 00:31:55 -0400

    feat: show the transaction summary in the detail panel

--- src/components/TransactionDetail/TransactionDetail.tsx
+++ src/components/TransactionDetail/TransactionDetail.tsx
@@ -1,6 +1,8 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import './TransactionDetail.css'
 import type { Transaction } from '../../types/transaction'
+import { StatusBadge } from '../StatusBadge/StatusBadge'
+import { amountTone, formatAmount } from '../../utils/transactions'
 
 type TransactionDetailProps = {
   transaction: Transaction | undefined
@@ -19,12 +21,27 @@ export function TransactionDetail({ transaction, onClose }: TransactionDetailPro
 
         <Dialog.Content className='detail-panel'>
           {transaction && (
-            <header className='detail-panel__header'>
-              <Dialog.Title className='detail-panel__title'>{transaction.merchant}</Dialog.Title>
-              <Dialog.Close className='detail-panel__close' aria-label='Close'>
-                &times;
-              </Dialog.Close>
-            </header>
+            <>
+              <header className='detail-panel__header'>
+                <Dialog.Title className='detail-panel__title'>{transaction.merchant}</Dialog.Title>
+                <Dialog.Close className='detail-panel__close' aria-label='Close'>
+                  &times;
+                </Dialog.Close>
+              </header>
+
+              <div className='detail-panel__body'>
+                <div className='detail-panel__hero'>
+                  <p className={`detail-panel__amount detail-panel__amount--${amountTone(transaction)}`}>
+                    {formatAmount(transaction.amount)}
+                  </p>
+                  {transaction.status !== 'posted' && <StatusBadge status={transaction.status} />}
+                </div>
+
+                <Dialog.Description className='detail-panel__description'>
+                  {transaction.description}
+                </Dialog.Description>
+              </div>
+            </>
           )}
         </Dialog.Content>
       </Dialog.Portal>
